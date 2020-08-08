8694ec666f3cd6d13131bf2dc328434f0fce4637
rkterungwa16 <terungwakombol@gmail.com>
2020-08-08 04:29:26 +0100

    [test] task reducer to fetch todays tasks

--- src/reducers/task.test.ts
+++ src/reducers/task.test.ts
@@ -28,7 +28,7 @@ describe.only("Task Reducer", () => {
     ).toEqual(mockTaskState);
   });
 
-  it("should return fetched tasks", () => {
+  it("should return fetched project tasks", () => {
     const mockTaskState = {
       ...defaultTasksState,
       tasks: defaultTasksState.tasks,
@@ -70,6 +70,75 @@ describe.only("Task Reducer", () => {
       tasksReducer(defaultTasksState, {
         type: TaskActions.FETCH_PROJECT_TASKS_FAILURE,
         error: "error"
+      })
+    ).toEqual(mockTaskState);
+  });
+
+  it("should make return true for requests to fetch todays tasks", () => {
+    const mockTaskState = {
+      ...defaultTasksState,
+      actions: {
+        ...defaultTasksState.actions,
+        fetchTodaysTasks: {
+          ...defaultTasksState.actions.fetchTodaysTasks,
+          isRequesting: true
+        }
+      }
+    };
+
+    expect(
+      tasksReducer(defaultTasksState, {
+        type: TaskActions.FETCH_TODAYS_TASKS
+      })
+    ).toEqual(mockTaskState);
+  });
+
+  it("should return fetched tasks for today", () => {
+    const mockTaskState = {
+      ...defaultTasksState,
+      tasks: defaultTasksState.tasks,
+      actions: {
+        ...defaultTasksState.actions,
+        fetchTodaysTasks: {
+          ...defaultTasksState.actions.fetchTodaysTasks,
+          isRequesting: false
+        }
+      }
+    };
+
+    expect(
+      tasksReducer(defaultTasksState, {
+        type: TaskActions.FETCH_TODAYS_TASKS_SUCCESS,
+        data: {
+          data: {
+            tasks: defaultTasksState.tasks
+          }
+        }
+      })
+    ).toEqual(mockTaskState);
+  });
+
+  it("should return error for unsuccessful fetch of tasks", () => {
+    const mockTaskState = {
+      ...defaultTasksState,
+      code: 400,
+      actions: {
+        ...defaultTasksState.actions,
+        fetchTodaysTasks: {
+          ...defaultTasksState.actions.fetchTodaysTasks,
+          isRequesting: false,
+          error: "error"
+        }
+      }
+    };
+
+    expect(
+      tasksReducer(defaultTasksState, {
+        type: TaskActions.FETCH_TODAYS_TASKS_FAILURE,
+        error: {
+          code: 400,
+          message: "error"
+        }
       })
     ).toEqual(mockTaskState);
   });
